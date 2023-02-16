dbeb18e460c878bc8485f3b46929c305e8c2ff45
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2023-02-16 13:15:10 +0000

    Correct user model

--- models/User.js
+++ models/User.js
@@ -2,24 +2,28 @@ const { Schema, model } = require('mongoose');
 
 // TODO: add user properties and validations according to assignment
 const userSchema = new Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true,
-        minLength: [3, 'Username must be at least 3 characters long!']
+    email: {
+        type:String,
+        requred: true,
+        validate: /^[a-zA-z0-9]+@[a-zA-z]+.[a-zA-z]+/i
     },
     hashedPassword: {
         type: String,
-        required: true
+        required: true,
+    },
+    firstName: {
+        type: String,
+        required: true,
+        minLength: [1, 'First name must be at least 1 character long!']
+    },
+    lastName:{
+        type:String,
+        required: true,
+        minLength: [1, 'Last name must be at least 1 character long!']
     }
 });
 
-userSchema.index({ username: 1 }, {
-    collation: {
-        locale: 'en',
-        strength: 2
-    }
-});
+
 
 const User = model('User', userSchema);
 
